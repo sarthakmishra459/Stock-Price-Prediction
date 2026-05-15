2da231712b77839430e84e8350465e8677b3ac6c
Sarthak Mishra <124512392+sarthakmishra459@users.noreply.github.com>
2026-05-15 19:45:54 +0530

    feat(models): move models into backend

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,9 +1,14 @@
 import os
 
 WINDOW = 30
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-MODEL_DIR = os.path.join(BASE_DIR, "..", "models")
-MODEL_DIR = os.path.abspath(MODEL_DIR)
+
+BASE_DIR = os.path.dirname(
+    os.path.dirname(
+        os.path.dirname(os.path.abspath(__file__))
+    )
+)
+
+MODEL_DIR = os.path.join(BASE_DIR, "models")
 
 MODEL_AVAILABILITY = {
     "lstm-gru": ["AAPL", "GOOGL", "AMZN", "AMD", "MSFT", "INTC", "IBM"],
